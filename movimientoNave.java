88353f92f82389d204f851ad1f8912837628f9cd
DeTeX360 <35928513+DeTeX360@users.noreply.github.com>
2025-10-20 20:06:13 -0300

    Create movimientoNave.java

---
+++ movimientoNave.java
@@ -0,0 +1,6 @@
+        	xVel = 0;
+        	yVel = 0;
+        	if (Gdx.input.isKeyPressed(Input.Keys.LEFT))  xVel = -3;
+        	if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) xVel =  3;
+        	if (Gdx.input.isKeyPressed(Input.Keys.DOWN))  yVel = -3;
+        	if (Gdx.input.isKeyPressed(Input.Keys.UP))    yVel =  3;
