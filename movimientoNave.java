a23f44eba152d15b570d885009ff28bac0dee26c
DeTeX360 <35928513+DeTeX360@users.noreply.github.com>
2025-10-22 22:47:31 -0300

    Delete movimientoNave.java

--- movimientoNave.java
+++
@@ -1,6 +0,0 @@
-        	xVel = 0;
-        	yVel = 0;
-        	if (Gdx.input.isKeyPressed(Input.Keys.LEFT))  xVel = -3;
-        	if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) xVel =  3;
-        	if (Gdx.input.isKeyPressed(Input.Keys.DOWN))  yVel = -3;
-        	if (Gdx.input.isKeyPressed(Input.Keys.UP))    yVel =  3;
