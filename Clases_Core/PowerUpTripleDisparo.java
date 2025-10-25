ab059c0c705088104852714de5a6fee58faf091a
DeTeX360 <35928513+DeTeX360@users.noreply.github.com>
2025-10-24 21:04:06 -0300

    Add PowerUpTripleDisparo class for triple shot effect

---
+++ Clases_Core/PowerUpTripleDisparo.java
@@ -0,0 +1,25 @@
+package puppy.code;
+import com.badlogic.gdx.math.Rectangle;
+
+public class PowerUpTripleDisparo extends PowerUp {
+
+    public PowerUpTripleDisparo() {
+        super("PowerTriple.png");
+        
+    }
+
+    @Override
+    public void aplicarEfecto(Object jugador) {
+    	
+        if (jugador instanceof Nave4) {
+            Nave4 nave = (Nave4) jugador;
+            nave.activarTripleDisparo();
+            nave.cambiarModelo("NaveDisparoMultiple.png");
+            desactivar();
+        }
+    }
+
+    public Rectangle getHitbox() {
+        return hitbox;
+    }
+}
