d2cb9a27742ffb178b17aebbce9abefcf6f4aefc
CristobalFloresH <cristobal.flores.h@mail.pucv.cl>
2025-11-21 01:35:55 -0300

    Update PowerUpTripleDisparo.java

--- Clases_Core/PowerUpTripleDisparo.java
+++ Clases_Core/PowerUpTripleDisparo.java
@@ -1,18 +1,27 @@
 package puppy.code;
+import com.badlogic.gdx.math.Rectangle;
 
 public class PowerUpTripleDisparo extends PowerUp {
 
     public PowerUpTripleDisparo() {
         super("PowerTriple.png");
+        
     }
 
     @Override
-    protected void aplicarEfecto(Nave4 nave) {
-        nave.activarTripleDisparo();
+    public void aplicarEfecto(Object jugador) {
+    	
+        if (jugador instanceof Nave4) {
+            Nave4 nave = (Nave4) jugador;
+            nave.activarTripleDisparo();
+            nave.cambiarModelo("NaveDisparoMultiple.png");
+            desactivar();
+        }
     }
 
-    @Override
-    protected String getModeloNave() {
-        return "NaveDisparoMultiple.png";
+    public Rectangle getHitbox() {
+        return hitbox;
     }
+
+
 }
