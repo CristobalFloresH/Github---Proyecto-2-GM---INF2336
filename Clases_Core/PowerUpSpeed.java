2b79a397a701c96c857fea0f34df6fe5b5ea6c99
MauriCrafter <maurix.koko@gmail.com>
2025-11-20 22:11:23 -0300

    Enhance PowerUpSpeed with boost effect and description

--- Clases_Core/PowerUpSpeed.java
+++ Clases_Core/PowerUpSpeed.java
@@ -2,21 +2,29 @@ package puppy.code;
 
 public class PowerUpSpeed extends PowerUp {
 
+    private float boost = 2.0f;
+
     public PowerUpSpeed() {
         super("PowerSpeed.png");
     }
 
     @Override
     protected void aplicarEfecto(Nave4 nave) {
+        nave.setVelocidad(nave.getVelocidad() * boost);
     }
 
     @Override
-    protected String getModeloNave() {
-        return "NaveVelocidad.png";
+    protected void aplicarCambioVisual(Nave4 nave) {
+        // en este caso no cambia textura 
     }
 
     @Override
-    protected void modificarValoresNave(Nave4 nave) {
-        nave.aumentarVelocidad();
+    protected void finalizarAplicacion(Nave4 nave) {
+        // para darle estructura 
+    }
+
+    @Override
+    public String getDescripcion() {
+        return "Incrementa la velocidad de movimiento.";
     }
 }
