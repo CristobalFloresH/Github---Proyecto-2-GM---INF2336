58f729441be71afce1b2f7111d57bd36a4fc2827
MauriCrafter <maurix.koko@gmail.com>
2025-11-21 12:32:45 -0300

    Refactor aplicarEfecto method in PowerUpEscudo
    Refactor PowerUpEscudo to use specific effect method

--- Clases_Core/PowerUpEscudo.java
+++ Clases_Core/PowerUpEscudo.java
@@ -1,24 +1,11 @@
-package puppy.code;
-import com.badlogic.gdx.math.Rectangle;
-
 public class PowerUpEscudo extends PowerUp {
-
+    
     public PowerUpEscudo() {
         super("PowerEscudo.png");
     }
 
     @Override
-    public void aplicarEfecto(Object jugador) {
-        if (jugador instanceof Nave4) {
-            Nave4 nave = (Nave4) jugador;
-            nave.restaurarModelo(); 
-            nave.activarEscudo();
-            desactivar();
-        }
+    protected void aplicarEfectoEspecifico(Nave4 nave) {
+        nave.activarEscudo();
     }
-
-    public Rectangle getHitbox() {
-        return hitbox;
-    }
-
 }
