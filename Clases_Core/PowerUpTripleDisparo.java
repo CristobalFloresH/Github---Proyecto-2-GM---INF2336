913b8e99d3e948cadc46d9b09581557bd0f72fcb
MauriCrafter <maurix.koko@gmail.com>
2025-11-20 21:58:47 -0300

    Refactor PowerUpTripleDisparo methods
    Refactor aplicarEfecto to accept Nave4 directly and update getModeloNave method.

--- Clases_Core/PowerUpTripleDisparo.java
+++ Clases_Core/PowerUpTripleDisparo.java
@@ -1,25 +1,18 @@
 package puppy.code;
-import com.badlogic.gdx.math.Rectangle;
 
 public class PowerUpTripleDisparo extends PowerUp {
 
     public PowerUpTripleDisparo() {
         super("PowerTriple.png");
-        
     }
 
     @Override
-    public void aplicarEfecto(Object jugador) {
-        if (jugador instanceof Nave4) {
-            Nave4 nave = (Nave4) jugador;
-            nave.restaurarModelo(); 
-            nave.activarTripleDisparo();
-            nave.cambiarModelo("NaveDisparoMultiple.png");
-            desactivar();
-        }
+    protected void aplicarEfecto(Nave4 nave) {
+        nave.activarTripleDisparo();
     }
 
-    public Rectangle getHitbox() {
-        return hitbox;
+    @Override
+    protected String getModeloNave() {
+        return "NaveDisparoMultiple.png";
     }
 }
