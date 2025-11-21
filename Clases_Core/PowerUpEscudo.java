859a6db887e8dd0facf4dd20683661ad8f32a8e9
MauriCrafter <maurix.koko@gmail.com>
2025-11-20 22:15:08 -0300

    Refactor PowerUpEscudo methods and return values

--- Clases_Core/PowerUpEscudo.java
+++ Clases_Core/PowerUpEscudo.java
@@ -7,27 +7,12 @@ public class PowerUpEscudo extends PowerUp {
     }
 
     @Override
-    protected void prepararAplicacion(Nave4 nave) {
-        // por si le agregaramos un reproducir sonido o efecto de partículas
-    }
-
-    @Override
     protected void aplicarEfecto(Nave4 nave) {
         nave.activarEscudo();
     }
 
     @Override
-    protected String obtenerModeloNave() {
-        return "NaveConEscudo.png";  
-    }
-
-    @Override
-    protected void finalizarAplicacion(Nave4 nave) {
-        // por si se necesita, solo es para la estructura
-    }
-
-    @Override
-    public String getDescripcion() {
-        return "Otorga un escudo protector a la nave.";
+    protected String getModeloNave() {
+        return null;
     }
 }
