88fa3225c9060160b6f215b41516476468ad7ad0
MauriCrafter <maurix.koko@gmail.com>
2025-11-21 12:33:02 -0300

    Update PowerUpSpeed.java

--- Clases_Core/PowerUpSpeed.java
+++ Clases_Core/PowerUpSpeed.java
@@ -1,28 +1,16 @@
-package puppy.code;
-import com.badlogic.gdx.math.Rectangle;
-
 public class PowerUpSpeed extends PowerUp {
-
+    
     public PowerUpSpeed() {
-        super("PowerSpeed.png");         
-
+        super("PowerSpeed.png");
     }
 
     @Override
-    public void aplicarEfecto(Object jugador) {
-        if (jugador instanceof Nave4) {
-            Nave4 nave = (Nave4) jugador;
-            nave.restaurarModelo(); 
-            nave.cambiarModelo("NaveVelocidad.png");
-            nave.aumentarVelocidad();
-            desactivar();
-        }
+    protected void aplicarEfectoEspecifico(Nave4 nave) {
+        nave.aumentarVelocidad();
     }
 
-    
-    public Rectangle getHitbox() {
-        return hitbox;
+    @Override
+    protected String obtenerModeloNave() {
+        return "NaveVelocidad.png";
     }
-
-
 }
